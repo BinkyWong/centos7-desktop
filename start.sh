a7e378a74d9bc0b99d2dada4fa96ad8dd0c76468
binky <binky@nuc.local>
2018-07-29 22:27:53 +0100

    enabled sudo for user and added an intellij installer

--- start.sh
+++ start.sh
@@ -3,9 +3,10 @@
 __create_user() {
 # Create a user to SSH into as.
 useradd user
-SSH_USERPASS=newpass
+SSH_USERPASS=password
 echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin user)
 echo ssh user password: $SSH_USERPASS
+usermod -aG wheel user
 }
 
 # Call all functions
